825e760c380c46708f52597ca330d6b2f22ee90a
Bobo Tang <bobo.tang@prizm.digital>
2021-11-29 16:19:24 +0800

    updated api baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -8,9 +8,10 @@ export default class API {
     };
 
     let url =
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:8080/"
-        : "http://zora3.herokuapp.com/";
+      // process.env.NODE_ENV === "development"
+      //   ? "http://localhost:8080/"
+      //   :
+      "https://zora3.herokuapp.com/";
     this.api = axios.create({
       baseURL: url,
       timeout: 10000,
